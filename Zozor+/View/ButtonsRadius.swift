dafe2b48c2ef218d553ffb24cc7c6896272b6ba9
DrKrakus <jeromekrakus@macbook-pro-de-jerome.home>
2018-12-27 09:17:12 +0100

    First commit

---
+++ Zozor+/View/ButtonsRadius.swift
@@ -0,0 +1,21 @@
+//
+//  ButtonsRadius.swift
+//  CountOnMe
+//
+//  Created by Jerome Krakus on 20/12/2018.
+//  Copyright © 2018 Ambroise Collon. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable extension UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+}
