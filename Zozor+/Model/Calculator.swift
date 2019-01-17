1db3404c826469d8b73923bb925e4bf8c1f2db12
DrKrakus <jeromekrakus@macbook-pro-de-jerome.home>
2019-01-17 17:59:18 +0100

    Making the MVC design pattern

---
+++ Zozor+/Model/Calculator.swift
@@ -0,0 +1,74 @@
+//
+//  Calculator.swift
+//  CountOnMe
+//
+//  Created by Jerome Krakus on 09/01/2019.
+//  Copyright © 2019 Ambroise Collon. All rights reserved.
+//
+
+import Foundation
+
+class Calculator {
+    // MARK: - Properties
+    var stringNumbers: [String] = [String()]
+    var operators: [String] = ["+"]
+    var index = 0
+    var total = 0
+    
+    var isExpressionCorrect: Bool {
+        if let stringNumber = stringNumbers.last {
+            if stringNumber.isEmpty {
+                return false
+            }
+        }
+        return true
+    }
+    
+    var canAddOperator: Bool {
+        if let stringNumber = stringNumbers.last {
+            if stringNumber.isEmpty {
+                return false
+            }
+        }
+        return true
+    }
+    
+    // MARK: - Methods
+    func addNewNumber(_ newNumber: Int) {
+        if let stringNumber = stringNumbers.last {
+            var stringNumberMutable = stringNumber
+            stringNumberMutable += "\(newNumber)"
+            stringNumbers[stringNumbers.count-1] = stringNumberMutable
+        }
+    }
+    
+    func plus() {
+        operators.append("+")
+        stringNumbers.append("")
+    }
+    
+    func minus() {
+        operators.append("-")
+        stringNumbers.append("")
+    }
+    
+    func calculeTotal() {
+        for (i, stringNumber) in stringNumbers.enumerated() {
+            if let number = Int(stringNumber) {
+                if operators[i] == "+" {
+                    total += number
+                } else if operators[i] == "-" {
+                    total -= number
+                }
+            }
+        }
+    }
+    
+    func clear() {
+        stringNumbers = [String()]
+        operators = ["+"]
+        index = 0
+        total = 0
+    }
+    
+}
